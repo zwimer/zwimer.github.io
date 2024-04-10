2a9f91bc9620266a5abdba8f65f93659fcf27f14
zwimer <zwimer@gmail.com>
2024-04-10 12:28:01 -0400

    email

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,7 +9,7 @@ $(function() {
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
-            
+
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/zwimer@gmail.com",
+                url: "https://formspree.io/contact.website@zwimer.com",
                 type: "POST",
                 data: {
 					name: name,
